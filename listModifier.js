484a51c54d79352f679518ad492f6104138f5b36
JaceRiehl <jace.riehl@gmail.com>
2018-01-03 23:17:36 -0700

    Clicking on list elements links with checked now and added style changed

--- listModifier.js
+++ listModifier.js
@@ -1,29 +1,25 @@
 window.onload=function(){
 	// Add a "checked" symbol when clicking on a list item
 	var list = document.querySelector('UL');
-	console.log(list + "HEY")
 	list.addEventListener('click', function(ev) {
-		document.body.style.backgroundColor = "red";
 		if (ev.target.tagName === 'LI') {
 			ev.target.classList.toggle('checked');
 		}
 	},false);
 
-
-	function addItemToList(){
-		var tdList = document.createElement("li");
-		var inputValue = document.getElementById("inputItem").value;
-		var tNode = document.createTextNode(inputValue);
-		tdList.appendChild(tNode);
-		if (inputValue === '') {
-		alert("Please enter something");
-		} else {
-		document.getElementById("toDoUl").appendChild(tdList);
-		}
-		document.getElementById("inputItem").value = '';
-	}
 }
-
+function addItemToList(){
+	var tdList = document.createElement("li");
+	var inputValue = document.getElementById("inputItem").value;
+	var tNode = document.createTextNode(inputValue);
+	tdList.appendChild(tNode);
+	if (inputValue === '') {
+	alert("Please enter something");
+	} else {
+	document.getElementById("toDoUl").appendChild(tdList);
+	}
+	document.getElementById("inputItem").value = '';
+}
 
 
 
