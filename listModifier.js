b25c9649ac8fe35f01b099d5ef15640cbbc0ef19
JaceRiehl <jace.riehl@gmail.com>
2018-01-03 23:04:54 -0700

    Fixed issue of queryselector returning null

--- listModifier.js
+++ listModifier.js
@@ -1,25 +1,27 @@
+window.onload=function(){
+	// Add a "checked" symbol when clicking on a list item
+	var list = document.querySelector('UL');
+	console.log(list + "HEY")
+	list.addEventListener('click', function(ev) {
+		document.body.style.backgroundColor = "red";
+		if (ev.target.tagName === 'LI') {
+			ev.target.classList.toggle('checked');
+		}
+	},false);
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-	document.body.style.backgroundColor = "red";
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-},false);
 
-
-function addItemToList(){
-	var tdList = document.createElement("li");
-	var inputValue = document.getElementById("inputItem").value;
-	var tNode = document.createTextNode(inputValue);
-	tdList.appendChild(tNode);
-	if (inputValue === '') {
-	alert("Please enter something");
-	} else {
-	document.getElementById("toDoUl").appendChild(tdList);
+	function addItemToList(){
+		var tdList = document.createElement("li");
+		var inputValue = document.getElementById("inputItem").value;
+		var tNode = document.createTextNode(inputValue);
+		tdList.appendChild(tNode);
+		if (inputValue === '') {
+		alert("Please enter something");
+		} else {
+		document.getElementById("toDoUl").appendChild(tdList);
+		}
+		document.getElementById("inputItem").value = '';
 	}
-	document.getElementById("inputItem").value = '';
 }
 
 
