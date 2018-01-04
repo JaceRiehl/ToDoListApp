4dcb12b9b500f9486f8fb0f31ed7a7ad1023a2ed
JaceRiehl <jace.riehl@gmail.com>
2018-01-03 19:14:04 -0700

    Added the css for a checkmark beside the list element

--- listModifier.js
+++ listModifier.js
@@ -1,13 +1,28 @@
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+	document.body.style.backgroundColor = "red";
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+},false);
+
+
 function addItemToList(){
 	var tdList = document.createElement("li");
 	var inputValue = document.getElementById("inputItem").value;
 	var tNode = document.createTextNode(inputValue);
-	tdList.appendChild(tNode) 
+	tdList.appendChild(tNode);
 	if (inputValue === '') {
-    alert("Please enter something");
+	alert("Please enter something");
 	} else {
-    document.getElementById("toDoUl").appendChild(tdList);
+	document.getElementById("toDoUl").appendChild(tdList);
 	}
-    document.getElementById("inputItem").value = '';
-  
+	document.getElementById("inputItem").value = '';
 }
+
+
+
+
+
